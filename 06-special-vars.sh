c2a026722ad0ea5ed8990f2cbfcccc0277e92b04
Muazam-khan <kmuazam6@gmail.com>
2023-11-19 18:54:59 -0500

    Added new lines

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -4,4 +4,7 @@
 
 # $0 has special privilege to print the name of script u r running
 
-echo "Printing script name $0"
+echo "Printing script Name :  $0"
+
+echo "First Argument is : $1 "
+echo "Second Argument is : $2 "
