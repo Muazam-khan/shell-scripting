cdbf8734fdae703db929c95525afcd6aa80a471e
Muazam-khan <kmuazam6@gmail.com>
2023-11-19 18:43:07 -0500

    Added new lines

--- 05-dynamic-vars.sh
+++ 05-dynamic-vars.sh
@@ -2,7 +2,7 @@
 
 # DATE="17JULY2023"
 
-DATE=$(date +%A)
+DATE=$(date +%x)
 NO_OF_SESSIONS=$(who|wc -l)
-echo -e "Good Morning , Today's date is \e[37m $DATE \e[0m" 
-echo -e "Total Number Of Connected Sessions : \e[37m $NO_OF_SESSIONS \e[0m"
+echo -e "Good Morning , Today's date is \e[31m $DATE \e[0m" 
+echo -e "Total Number Of Connected Sessions : \e[31m $NO_OF_SESSIONS \e[0m"
