d484a3b830af65f5c62e2a856b6def3979ef995a
Muazam-khan <kmuazam6@gmail.com>
2023-11-19 16:54:19 -0500

    Added new lines

--- 02-esc-sequence.sh
+++ 02-esc-sequence.sh
@@ -3,6 +3,15 @@
 echo Welcome To Shell Scripting
 echo "This is day 1 of our shell scripting"
 
+# Escape Sequence characters
+
+#  \n : New Line 
+# \t : tab Space 
+
 echo -e "Line1\nLine2"
-echo -e "Line3\t Line5"
-echo Line4 
+echo -e "Line3\tLine4"
+echo Line5
+
+# Whenever you're using special characters , always enclose them in DOUBLE QUOTES
+# " "  : Double Quotes 
+# ' '  : Single Quotes
