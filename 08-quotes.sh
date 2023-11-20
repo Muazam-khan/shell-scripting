03f7c65dfb97a1140cc4014dcd61a15b2a26be96
Muazam-khan <kmuazam6@gmail.com>
2023-11-19 19:43:00 -0500

    Added new lines

--- 08-quotes.sh
+++ 08-quotes.sh
@@ -3,6 +3,6 @@
 echo "$$"    # $$  is going to print the PID of the current proces 
 echo '$$'    # Single Quotes Always Eliminates The Power Of The Special Variable 
 
-echo $? 
-echo "$?"
-echo '$?'
+echo $?      # This prints the exit code of the last command
+echo "$?"     # This prints the exit code of the last command 
+echo '$?'     # This should not print the exit code of the last command as single quote killed the power
