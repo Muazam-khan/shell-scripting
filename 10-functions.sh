1f4f0fde3c3bf1638a6238cfa332e9bf29d00995
Muazam-khan <kmuazam6@gmail.com>
2023-11-19 21:05:18 -0500

    Added new lines

--- 10-functions.sh
+++ 10-functions.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-# There are 4 types of command available : 
+# There are 4 types of commands available : 
 
 # 1) Binary                   ( /bin  , /sbin )
-# 2) Aliases                  ( Alises are shortcuts,  alias net="netstat -tulpn" )
+# 2) Aliases                  ( Alises are shortcuts,  alias net="netstat -tulpn", ls is an alias command, gp is an alias cmd )
 # 3) Shell Built-in Commands  
-# 4) Functions                # Functions are nothing but a set of command that can be written in a sequence and can be called n number of times as per your choice.
+# 4) Functions           # Functions are nothing but a set of commands that can be written in a sequence and can be called n number of times as per your choice.
 
 
 # How to declare a function ?
 
 f() {
-    echo hai
+    echo Hye
 }
-
 # How do you call a function 
 
 f   # calling a function
@@ -23,15 +22,13 @@ f   # calling a function
 #     echo Sample function is completed
 # }
 
-
 # sample 
 
 # echo sample call is completed 
 
 # sample 
 
-
-b56() {
+b56() {   # function declared here
     echo "This is our batch56 function"
     echo "We are learning functions"
     echo "Todays date is:"
@@ -40,16 +37,16 @@ b56() {
 }
 
 
-b56 
+b56 # function is called here
 
 
-stat() {
+stat() { # function stat being declared
 
-    echo "Number of sessions opened $(who|wc -l)"
-    echo "Todays date is $(date +%F)"
+    echo "Number of sessions opened $(who|wc -l)" #who displays session names, wc -l gives no of those
+    echo "Todays date is $(date +%F)" # date cmd gives date
     echo "AVG Cpu Utilization In The Last 5 minues is : $(uptime|awk -F : '{print $NF}' | awk -F ',' '{print $2}')"
-
+  #ist field separator was : and last field separt was comma and in last pipe we wanted 2nd argument of uptime to know 5 min
     b56  # calling a function from another function
 }
 
-stat 
+stat  # called function stat here
