3da5107eda2f6f429a78d2ec7edfb820d3e6cdce
Muazam-khan <kmuazam6@gmail.com>
2023-11-21 18:24:43 -0500

    Added new lines

--- 13-conditions.sh
+++ 13-conditions.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
 # conditions help us to execute something only if some factor is True/False
+
+#syntax of case
+ ACTION=$1
+
+case $ACTION in
+    start)
+        echo -e "\e[32m Starting shipping service \e[om"
+        exit 0
+        ;;
+    stop)
+        echo -e "\e[31m Stoppng shipping service \e[om"
+        exit 1
+        ;;
+    restart)
+        echo -e "\e[33m Restarting shipping service \e[om"
+        exit 2
+        ;;
+    *)
+        echo -e "\e[32m Valid options are start-stop-restart only \e[om"
+        echo -e "\e[36m Example usage: \e[om bash script.sh start"
+        exit 3
+esac         
