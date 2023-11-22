57b9117bad93fce60db4596f2c77ea77addbe003
Muazam-khan <kmuazam6@gmail.com>
2023-11-21 19:01:57 -0500

    Added new lines

--- 14-ifelse-syntax.sh
+++ 14-ifelse-syntax.sh
@@ -3,7 +3,7 @@
 
 # echo -e "Demo on if, if else, else if usage
   ACTION=$1
-if  [ "$ACTION" == "start" ] ; then
+if  [ $ACTION == start ] ; then
   echo "Starting shipping service"
   exit 0
 fi   
