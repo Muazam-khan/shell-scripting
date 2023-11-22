76da89a34a3f814b0edea4b145c7725ef2ee3f1d
Muazam-khan <kmuazam6@gmail.com>
2023-11-21 19:26:21 -0500

    Added new lines

--- 15-ifelse.sh
+++ 15-ifelse.sh
@@ -6,7 +6,10 @@ if [ $ACTION == start ] ; then
 
  echo -e "\e[32m Starting Shipping Service \e[0m"
  exit 0
+else
+   echo -e "\e[31m Stoppng Shipping Service \e[0m"
+   exit 1
  fi
 
- echo "\e[34m Not met any conditions\e[om"
+ echo -e "\e[34m Not met any conditions\e[om"
  exit 100
