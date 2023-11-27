cae83c1b8a58068a56dadbef75605c1ab421f671
Muazam-khan <kmuazam6@gmail.com>
2023-11-26 19:19:45 -0500

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -46,9 +46,7 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
 
 echo -n "Updating Reverse Proxy: "
-sed -i -e "/catalogue/s/localhost/catalogue.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
-sed -i -e "/user/s/localhost/user.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
-sed -i -e "/cart/s/localhost/cart.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
+sed -i -e "/catalogue/s/localhost/catalogue.roboshop.internal/" -e "/user/s/localhost/user.roboshop.internal/" -e "/cart/s/localhost/cart.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
 stat $?
 
 echo -n "Restarting $COMPONENT :"
