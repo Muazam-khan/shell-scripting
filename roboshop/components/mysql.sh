d0b27fab9fbd4429408815ac9998509d559d5bec
Muazam-khan <kmuazam6@gmail.com>
2023-11-28 13:41:18 -0500

    Auto Push

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -1,7 +1,22 @@
 #!bin/bash
 
 COMPONENT=mysql
+COMPONENT_URL="https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo"
 
 source components/common.sh
 
 echo -e *********"\e[31m configuring ${COMPONENT} \e[0m"*******
+
+echo -n "Configuring $COMPONENT repo:"
+curl -s -L -o /etc/yum.repos.d/mysql.repo $COMPONENT_URL &>> $LOGFILE
+stat $?
+
+echo -n "Installing $COMPONENT :"
+yum install mysql-community-server -y &>> $LOGFILE
+stat $?
+
+echo -n "Starting $COMPONENT :"
+systemctl enable mysqld &>> $LOGFILE
+systemctl daemon-reload &>> $LOGFILE
+systemctl start mysqld &>> $LOGFILE
+stat $?
