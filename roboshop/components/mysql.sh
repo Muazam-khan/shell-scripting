aa54296e5786137e026f4db48048144a3712de70
Muazam-khan <kmuazam6@gmail.com>
2023-11-28 16:57:09 -0500

    Auto Push

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -1,13 +1,15 @@
 #!bin/bash
 
 COMPONENT=mysql
+COMPONENT_URL="https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/${COMPONENT}.repo"
+SCHEMA_URL="https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 
 source components/common.sh
 
 echo -e *********"\e[31m configuring ${COMPONENT} \e[0m"*******
 
 echo -n "Configuring $COMPONENT repo:"
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo &>> $LOGFILE
+curl -s -L -o /etc/yum.repos.d/mysql.repo  &>> $LOGFILE $COMPONENT_URL
 stat $?
 
 echo -n "Installing $COMPONENT :"
@@ -37,3 +39,12 @@ if [ $? -eq 0 ]; then
     stat $?
 fi
    
+echo -n "Downloading the $COMPONENT schema :"
+curl -s -L -o /tmp/mysql.zip $SCHEMA_URL &>> $LOGFILE
+stat $?
+
+echo -n "Extracting $COMPONENT schema :"
+cd /tmp
+unzip ${COMPONENT}.zip
+cd ${COMPONENT}-main
+mysql -u root -pRoboShop@1 <shipping.sql
