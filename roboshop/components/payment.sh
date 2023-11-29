1cdd0c7d7381fea44c0a4cfa40ec127e0059eb38
Muazam-khan <kmuazam6@gmail.com>
2023-11-29 15:49:24 -0500

    Auto Push

--- roboshop/components/payment.sh
+++ roboshop/components/payment.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-COMPONENT=catalogue
+COMPONENT=payment
 
 echo -e *********"\e[31m configuring ${COMPONENT} \e[0m"*******
 
 source components/common.sh
 
-NODEJS
+PYTHON   # calls python func
 
 echo -e *********"\e[31m $COMPONENT Configuration is completed \e[0m"*******
