a55ac79a9398a4e7a2d1503b3bb5cbf4d187f131
Muazam-khan <kmuazam6@gmail.com>
2023-11-29 15:48:31 -0500

    Auto Push

--- roboshop/components/payment.sh
+++ roboshop/components/payment.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "configuring payment"
+COMPONENT=catalogue
+
+echo -e *********"\e[31m configuring ${COMPONENT} \e[0m"*******
+
+source components/common.sh
+
+NODEJS
+
+echo -e *********"\e[31m $COMPONENT Configuration is completed \e[0m"*******
--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -1,12 +1,10 @@
 #!bin/bash
 
 COMPONENT=rabbitmq
-#COMPONENT_URL="https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo"
-#SCHEMA_URL="https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 
 source components/common.sh
 
-echo -e *********"\e[31m configuring ${COMPONENT} \e[0m"*******
+echo -e *********"\e[34m configuring ${COMPONENT} \e[0m"*******
 
 echo -n "Configuring $COMPONENT repo:"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash &>> $LOGFILE 
@@ -22,7 +20,7 @@ systemctl enable rabbitmq-server &>> $LOGFILE
 systemctl start rabbitmq-server &>> $LOGFILE
 stat $?
 
-rabbitmqctl list_users | grep ${APPUSER}
+rabbitmqctl list_users | grep ${APPUSER} &>> $LOGFILE
 if [ $? -ne 0 ]; then
     echo -n "Creating $APPUSER for $COMPONENT:"
     rabbitmqctl add_user roboshop roboshop123 &>> $LOGFILE
@@ -34,4 +32,4 @@ rabbitmqctl set_user_tags roboshop administrator &>> $LOGFILE
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $LOGFILE
 stat $?
 
-echo -e *********"\e[31m $COMPONENT Configuration is completed \e[0m"*******
+echo -e *********"\e[34m $COMPONENT Configuration is completed \e[0m"*******
