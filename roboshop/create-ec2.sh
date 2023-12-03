ea25b39cb5cd6dad6451c8efec3019df3e6cbcff
Muazam-khan <kmuazam6@gmail.com>
2023-12-02 20:52:39 -0500

    Auto Push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -4,7 +4,7 @@
 
 #AMI_ID="ami-0f75a13ad2e340a58"  # hardcoding is a bad-choice particularly with AMI_ID as it is going to be changed when you register a new AMI.
 if [ -z $1 ] ; then
-    echo -e "\e[31m ****Component name is need ****\e[0m \n\t\t"
+    echo -e "\e[31m ****Component name is needed ****\e[0m \n\t\t"
     echo -e "\e[36m **\t\tExample Usage : \e[0m bash create-ec2 ratings"
     exit 1
 fi    
@@ -15,16 +15,16 @@ SGID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=b56-al
 INSTANCE_TYPE="t3.micro"
 
 create_server(){
-    echo -e "******* \e[32m $COMPONENT \e[0m Server Creation In Progress ******* !!!!!!! "
+    echo -e "******* \e[32m $COMPONENT  Server Creation In Progress \e[0m******* !!!!!!! "
     PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SGID} --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=${COMPONENT}}]" | jq ".Instances[].PrivateIpAddress" | sed -e 's/"//g')
-    echo -e "******* \e[32m $COMPONENT \e[0m Server Creation is completed ******* !!!!!!! \n\n"
+    echo -e "******* \e[32m $COMPONENT  Server Creation is completed \e[0m******* !!!!!!! \n\n"
 
-    echo -e  ******* "\e[32m $COMPONENT \e[0m DNS Record Creation In Progress ******* !!!!!!! "
+    echo -e  ******* "\e[32m $COMPONENT  DNS Record Creation In Progress \e[0m******* !!!!!!! "
     sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATE_IP}/" route53.json > tmp/dns.json
     
 
-    aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/dns.json\
-    echo -e  ******* "\e[32m $COMPONENT \e[0m DNS Record Creation In Completed ******* !!!!!!! \n\n"
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/dns.json
+    echo -e  ******* "\e[32m $COMPONENT  DNS Record Creation In Completed \e[0m******* !!!!!!! \n\n"
  }
   # if user supplies all as first argument then all servers will be created
  if [ "$1" == "all" ]; then
@@ -35,6 +35,6 @@ create_server(){
         done    
 
 else
-     create-ec2
+     create-server
 fi        
    
