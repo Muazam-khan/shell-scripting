2951116c00ab0ee2ebcc28a97c1dc2358468b73f
Muazam-khan <kmuazam6@gmail.com>
2023-12-02 21:01:21 -0500

    Auto Push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -15,6 +15,7 @@ SGID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=b56-al
 INSTANCE_TYPE="t3.micro"
 
 create_server(){
+
     echo -e "******* \e[32m $COMPONENT  Server Creation In Progress \e[0m******* !!!!!!! "
     PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SGID} --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=${COMPONENT}}]" | jq ".Instances[].PrivateIpAddress" | sed -e 's/"//g')
     echo -e "******* \e[32m $COMPONENT  Server Creation is completed \e[0m******* !!!!!!! \n\n"
@@ -23,8 +24,9 @@ create_server(){
     sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATE_IP}/" route53.json > tmp/dns.json
     
 
-    aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/dns.json
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file://tmp/dns.json
     echo -e  ******* "\e[32m $COMPONENT  DNS Record Creation In Completed \e[0m******* !!!!!!! \n\n"
+    
  }
   # if user supplies all as first argument then all servers will be created
  if [ "$1" == "all" ]; then
